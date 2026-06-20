fb085731dcf69d172e0168ca31adc0fb522b6c05
Dinh Bao Khanh <khanhdbao@gmail.com>
2026-06-20 08:46:22 +0700

    init: add fibo algorithm

---
+++ app.js
@@ -0,0 +1,17 @@
+function fibo(n) {
+    if (n <= 1) return n;
+    
+    let prev2 = 0;
+    let prev1 = 1;
+    let current = 0;
+    
+    for (let i = 2; i <= n; i++) {
+        current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+    
+    return current;
+}
+
+console.log(fibo(10));
